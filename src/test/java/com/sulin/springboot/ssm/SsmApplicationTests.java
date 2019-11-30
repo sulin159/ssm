a62e8a07386cf89651d0c53ab59e511f1b3e634d
sulin159 <484845401@qq.com>
2019-11-30 20:49:35 +0800

    Initial commit

---
+++ src/test/java/com/sulin/springboot/ssm/SsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulin.springboot.ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
